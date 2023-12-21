338251337f0f4d08d819809b7dcb8203a75b213a
Milan Stanisavljevic <milan.stanisavljevic@vega.local>
2023-12-21 17:15:46 +0100

    add reusable button

--- src/app/modules/shared/custom/custom-buttom/custom-buttom.component.ts
+++ src/app/modules/shared/custom/custom-buttom/custom-buttom.component.ts
@@ -1,34 +1,19 @@
-import { Component, Input, Provider, forwardRef } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
-
-const PROVIDER_CONFIG: Provider = {
-  provide: NG_VALUE_ACCESSOR,
-  useExisting: forwardRef(() => CustomButtomComponent),
-  multi: true,
-};
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { BtnType } from './types/enumClass.enum';
 
 @Component({
   selector: 'app-custom-buttom',
   templateUrl: './custom-buttom.component.html',
   styleUrls: ['./custom-buttom.component.scss'],
-  providers: [PROVIDER_CONFIG],
 })
-export class CustomButtomComponent implements ControlValueAccessor {
-  @Input() disabled: boolean = false;
-  onChange: any;
-  text: string = 'Click';
-  writeValue(obj: any): void {
-    this.text = obj;
-    console.log(obj);
+export class CustomButtomComponent {
+  @Input() text!: string;
+  @Input() type: string = 'button';
+  @Output() clickButton = new EventEmitter();
+  @Input() isDisabled: boolean = false;
+  @Input() buttonType: BtnType = 'primary';
+
+  onClick() {
+    this.clickButton.emit();
   }
-  registerOnChange(fn: any): void {
-    this.onChange = fn;
-  }
-  registerOnTouched(fn: any): void {
-    throw new Error('Method not implemented.');
-  }
-  setDisabledState?(isDisabled: boolean): void {
-    this.disabled = isDisabled;
-  }
-  onClick() {}
 }
---
+++ src/app/modules/shared/custom/custom-buttom/types/enumClass.enum.ts
@@ -0,0 +1,14 @@
+export enum ButtonType {
+  primary,
+  secondary,
+  warning,
+  success,
+  danger,
+}
+
+export type BtnType =
+  | 'primary'
+  | 'secondary'
+  | 'warning'
+  | 'success'
+  | 'danger';
