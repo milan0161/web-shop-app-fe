49183bc10755a3197ee68a768b8b1901fcd9d5c1
Milan Stanisavljevic <milan.stanisavljevic@vega.local>
2024-01-17 15:26:43 +0100

    change auth guard according to mentors comments

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,18 +1,9 @@
-import {inject} from "@angular/core";
-import {UserService} from "../../modules/user/user.service";
-import {map} from "rxjs";
-import {CanActivateFn} from "@angular/router";
-import {RouterService} from "../router/router.service";
-
+import { inject } from '@angular/core';
+import { UserService } from '../../modules/user/user.service';
+import { map } from 'rxjs';
+import { CanActivateFn } from '@angular/router';
+import { RouterService } from '../router/router.service';
 
 export function AuthGuard(): CanActivateFn {
-  return () => {
-    const userService: UserService = inject(UserService);
-    const customRouter: RouterService = inject(RouterService)
-    return userService.loggedUser$.pipe(map((user) => {
-      if (user) return true
-      customRouter.navigate('home')
-      return false
-    }))
-  }
+  return () => inject(UserService).loggedUser$.pipe(map((user) => !!user));
 }
