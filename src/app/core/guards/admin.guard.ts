7c11b4501b867e4b378af538b39c9766c2bfaa49
Milan016 <stanisavljevic07@gmail.com>
2024-01-24 22:28:44 +0100

    chage admin guard

--- src/app/core/guards/admin.guard.ts
+++ src/app/core/guards/admin.guard.ts
@@ -1,15 +1,12 @@
 import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
-import { filter, map } from 'rxjs';
+import {map} from 'rxjs';
 import { UserService } from 'src/app/modules/user/user.service';
 
 export function AdminGuard(): CanActivateFn {
-  return () =>
-    inject(UserService).loggedUser$.pipe(
-      filter((user) => {
-        console.log(user?.role === 'ADMINISTRATOR');
-        return user!.role === 'ADMINISTRATOR';
-      }),
-      map((user) => !!user)
-    );
+  return () => inject(UserService).loggedUser$.pipe(map(user => user?.role === 'ADMINISTRATOR'))
 }
+
+
+
+
