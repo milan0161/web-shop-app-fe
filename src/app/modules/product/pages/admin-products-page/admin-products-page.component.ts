66e20b3efdfe0684b8186430e54fad434b3a4e97
Milan Stanisavljevic <milan.stanisavljevic@vega.local>
2024-01-25 16:38:18 +0100

    add takeUntil

--- src/app/modules/product/pages/admin-products-page/admin-products-page.component.ts
+++ src/app/modules/product/pages/admin-products-page/admin-products-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ProductService } from '../../product.service';
 import { UserService } from 'src/app/modules/user/user.service';
-import { map, tap } from 'rxjs';
+import { Subject, takeUntil } from 'rxjs';
 import { DialogService } from 'src/app/modules/shared/custom/dialog/dialog.service';
 import { ProductFormComponent } from '../../product-form/product-form.component';
 
@@ -10,7 +10,8 @@ import { ProductFormComponent } from '../../product-form/product-form.component'
   templateUrl: './admin-products-page.component.html',
   styleUrls: ['./admin-products-page.component.scss'],
 })
-export class AdminProductsPageComponent {
+export class AdminProductsPageComponent implements OnDestroy {
+  private destroyed$ = new Subject<void>();
   products$ = this.productService.getProductsAdmin();
   currentUser$ = this.userService.loggedUser$;
   constructor(
@@ -23,10 +24,16 @@ export class AdminProductsPageComponent {
     this.dialogService
       .open<ProductFormComponent, any>(ProductFormComponent)
       .afterClosed()
+      .pipe(takeUntil(this.destroyed$))
       .subscribe((res) => {
         if (res) {
           this.products$ = this.productService.getProductsAdmin();
         }
       });
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 }
