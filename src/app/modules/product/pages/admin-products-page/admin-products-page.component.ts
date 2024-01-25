67c1237931f0b6c324930ecb0921cd233916f226
Milan Stanisavljevic <milan.stanisavljevic@vega.local>
2024-01-25 11:28:11 +0100

    add product

--- src/app/modules/product/pages/admin-products-page/admin-products-page.component.ts
+++ src/app/modules/product/pages/admin-products-page/admin-products-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../product.service';
 import { UserService } from 'src/app/modules/user/user.service';
 import { map, tap } from 'rxjs';
+import { DialogService } from 'src/app/modules/shared/custom/dialog/dialog.service';
+import { ProductFormComponent } from '../../product-form/product-form.component';
 
 @Component({
   selector: 'app-admin-products-page',
@@ -13,8 +15,18 @@ export class AdminProductsPageComponent {
   currentUser$ = this.userService.loggedUser$;
   constructor(
     private productService: ProductService,
-    private userService: UserService
+    private userService: UserService,
+    private dialogService: DialogService
   ) {}
 
-  addNewProduct() {}
+  addNewProduct() {
+    this.dialogService
+      .open<ProductFormComponent, any>(ProductFormComponent)
+      .afterClosed()
+      .subscribe((res) => {
+        if (res) {
+          this.products$ = this.productService.getProductsAdmin();
+        }
+      });
+  }
 }
